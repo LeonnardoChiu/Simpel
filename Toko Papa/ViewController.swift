9f25b8187201fd9c1bf86a1610e2ad24c0d147e3
louis <lrv1302@gmail.com>
2019-10-28 10:30:24 +0700

    Initial Commit

---
+++ Toko Papa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toko Papa
+//
+//  Created by Louis  Valen on 28/10/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
