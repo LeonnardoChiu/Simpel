47e4a856ba630d57b009e70dc46e04b6dc802f67
louis <lrv1302@gmail.com>
2019-11-04 11:51:09 +0700

    save barang ud bisa

---
+++ Toko Papa/AddInventoryViewController.swift
@@ -0,0 +1,50 @@
+//
+//  AddInventoryViewController.swift
+//  Toko Papa
+//
+//  Created by Louis  Valen on 04/11/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class AddInventoryViewController: UIViewController {
+    let database = CKContainer.default().publicCloudDatabase
+    
+    @IBOutlet weak var barcode: UITextField!
+    @IBOutlet weak var nameProduct: UITextField!
+    @IBOutlet weak var price: UITextField!
+    @IBOutlet weak var stock: UITextField!
+    @IBOutlet weak var distributorName: UITextField!
+    @IBOutlet weak var category: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func saveToCloud(Barcode: String, Name: String, Category:String, Distributor:String, Stock:Int, Price: Int){
+            let NewNote = CKRecord(recordType: "Inventory")//ini buat data base baru
+            NewNote.setValue(Barcode, forKey: "Barcode")//ini ke tablenya
+            NewNote.setValue(Category, forKey: "Category")
+            NewNote.setValue(Distributor, forKey: "Distributor")
+            NewNote.setValue(Name, forKey: "NameProduct")
+            NewNote.setValue(Price, forKey: "Price")
+            NewNote.setValue(Stock, forKey: "Stock")
+        
+         database.save(NewNote) { (record, error) in
+             print(error)
+             guard record != nil else { return}
+             print("savaedddd")
+         }
+    }
+
+    @IBAction func doneButton(_ sender: Any) {
+       print("buka save")
+        self.saveToCloud(Barcode: barcode.text!, Name: nameProduct.text!, Category: category.text!, Distributor: distributorName.text!, Stock: Int(stock.text!)!, Price: Int(price.text!)!)
+        
+    
+    }
+
+}
---
+++ Toko Papa/InventoryViewController.swift
@@ -0,0 +1,53 @@
+//
+//  InventoryViewController.swift
+//  Toko Papa
+//
+//  Created by Louis  Valen on 04/11/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class InventoryViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+     var data = [CKRecord]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PostView
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+class PostView: UITableViewCell{
+    
+}
