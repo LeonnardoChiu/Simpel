0e189b9889720c3cec452995efbce4321a776d7d
Ricky Erdiansyah <erdiansyah.ricky@gmail.com>
2019-11-06 15:19:53 +0700

    Employe List Page 	Segue, Passing Data (object) Employe Profile Page 	Show data Storyboard untuk edit & add

---
+++ Toko Papa/Controller/Access/Add New & Edit Employee/AddEmployeeCell.swift
@@ -0,0 +1,10 @@
+//
+//  AddEmployeeCell.swift
+//  Toko Papa
+//
+//  Created by Ricky Erdiansyah on 06/11/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
+
---
+++ Toko Papa/Controller/Access/Add New & Edit Employee/EditEmployeeCell.swift
@@ -0,0 +1,9 @@
+//
+//  EditEmployeeCell.swift
+//  Toko Papa
+//
+//  Created by Ricky Erdiansyah on 06/11/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import UIKit
