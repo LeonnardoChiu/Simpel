623dd04e7a694fae454d3c359a7d3e0c1b61c147
Leonnardo Benjamin Hutama <leonnardo.hutama@binusa.c.id>
2019-11-07 09:27:07 +0700

    report and calendar

---
+++ Toko Papa/Controller/Report/CalendarVars.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarVars.swift
+//  Toko Papa
+//
+//  Created by Leonnardo Benjamin Hutama on 06/11/19.
+//  Copyright © 2019 Louis  Valen. All rights reserved.
+//
+
+import Foundation
+
+let date = Date()
+let calendar = Calendar.current
+
+let day = calendar.component(.day, from: date)
+var weekday = calendar.component(.weekday, from: date) - 1
+var month = calendar.component(.month, from: date) - 1
+var year = calendar.component(.year, from: date)
