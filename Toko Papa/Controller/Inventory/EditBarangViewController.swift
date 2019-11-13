4b3b87867e979dab95821cf89a2396e5476bf1ed
louis <lrv1302@gmail.com>
2019-11-13 16:45:03 +0700

    rabu done

--- Toko Papa/Controller/Inventory/EditBarangViewController.swift
+++ Toko Papa/Controller/Inventory/EditBarangViewController.swift
@@ -8,12 +8,66 @@
 
 import UIKit
 
-class EditBarangViewController: UIViewController {
+class EditBarangViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    var satuanSekarang: String? = "Unit"
 
+    var placeHolderTextField: [String] = ["Barcode", "Nama Produk", "Kategori", "Distributor", "Stok"]
+
+    @IBOutlet weak var tableView: UITableView!{
+        didSet {
+            tableView.tableFooterView = UIView(frame: .zero)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 5
+        }else if section == 1 {
+            return 1
+        }else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellPrice = tableView.dequeueReusableCell(withIdentifier: "price", for: indexPath) as! TambahBarangCellPriceList
+           
+           let cellBiasa = tableView.dequeueReusableCell(withIdentifier: "biasa", for: indexPath) as! TambahBarangCellBiasa
+           let cells = UITableViewCell()
+           switch indexPath.section {
+           case 0:
+                   cellBiasa.tambahBarangTextField.placeholder = placeHolderTextField[indexPath.row]
+                   return cellBiasa
+           case 1:
+                   cellPrice.tambahBarangTextField.placeholder = "Harga per"
+                   cellPrice.PieceLabel.text = satuanSekarang
+                   cellPrice.accessoryType = .disclosureIndicator
+                   return cellPrice
+           default:
+                return cells
+           }
+           return cells
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return ""
+        }else if section == 1 {
+            return "Price List"
+        }
+        return ""
     }
     
 
